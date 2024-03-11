96d5ac4c730266668acd1e115a4d1da03d51eb88
KinataDewa <dewadafug766@gmail.com>
2024-03-11 23:53:13 +0700

    praktikum2.7

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserModel extends Model
 {
@@ -11,12 +12,19 @@ class UserModel extends Model
 
     protected $table = 'm_user'; // Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id'; // Mendifinisikan primary key dari tabel yang digunakan
-    /**
-     * The atributes that are mass assignable.
-     * 
-     * @var array
-     */
-    // protected $fillable = ['level_id', 'username', 'nama'];
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
 
+
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 }
+// /**
+//  * The atributes that are mass assignable.
+//  * 
+//  * @var array
+//  */
+// // protected $fillable = ['level_id', 'username', 'nama'];
+// protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -17,6 +17,8 @@
             <th>Username</th>
             <th>Nama</th>
             <th>ID Level Pengguna</th>
+            <td>Kode Level</td>
+            <td>Nama Level</td>
             <th>Aksi</th>
         </tr>
         @foreach ($data as $d)
@@ -25,6 +27,8 @@
             <td>{{ $d->Username}}</td>
             <td>{{ $d->nama}}</td>
             <td>{{ $d->level_id}}</td>
+            <td>{{ $d->level->level_kode }}</td>
+            <td>{{ $d->level->level_nama }}</td>
             <td><a href={{route('/user/ubah', $d->user_id) }}>Ubah</a> |
              <a href={{route('/user/hapus', $d->user_id)}}>Hapus</a></td>
         </tr>
