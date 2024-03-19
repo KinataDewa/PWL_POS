8fd4983b7d50646e44450843f2d62d41b04ee976
KinataDewa <dewadafug766@gmail.com>
2024-03-19 08:57:32 +0700

    commit

--- app/Models/KategoriModel.php
+++ app/Models/KategoriModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class KategoriModel extends Model
 {
@@ -12,8 +13,9 @@ class KategoriModel extends Model
 
     protected $fillable = ['kategori_kode', 'kategori_nama'];
 
-    public function barang()
+    use HasFactory;
+    public function barang(): HasMany
     {
-        $this->hasMany(BarangModel::class, 'barang_id', 'barang_id');
+        return $this->hasMany(BarangModel::class, 'barang_id', 'barang_id');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,16 @@ use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,12 +22,14 @@ Route::get('/', function () {
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
 Route::get('/user', [UserController::class, 'index'])->name('/user');
 Route::get('/user/tambah', [UserController::class, 'tambah'])->name('/user/tambah');
 Route::get('/user/ubah/{id}', [UserController::class, 'ubah'])->name('/user/ubah');
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus'])->name('/user/hapus');
 Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan'])->name('/user/tambah_simpan');
 Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan'])->name('/user/ubah_simpan');
-Route::get('/kategori/create', [KategoriController::class, 'create']);
+Route::get('/kategori/create', [KategoriController::class, 'create'])->name('TambahKategori');
 Route::post('/kategori', [KategoriController::class, 'store']);
+Route::get('/kategori/edit/{id}', [KategoriController::class, 'edit'])->name('EditKategori');
+Route::put('/kategori/update/{id}', [KategoriController::class, 'update'])->name('UpdateKategori');
+Route::get('/kategori/delete/{id}', [KategoriController::class, 'delete'])->name('DeleteKategori');
